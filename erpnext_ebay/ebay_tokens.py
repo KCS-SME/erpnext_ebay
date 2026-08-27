14ecfe78a13d5759e22e67346d9e50d3014dd3e9
Ankita Sharma <ankita@korecent.com>
2026-08-27 10:27:36 +0530

    Chore: Show meaningful error message on token exchange failure

--- erpnext_ebay/ebay_tokens.py
+++ erpnext_ebay/ebay_tokens.py
@@ -161,11 +161,23 @@ def accept_consent_token():
 
     # Process response
     if not r.ok:
-        frappe.throw('Invalid parameters!')
+        return frappe.respond_as_web_page(
+            title='Authorization Failed',
+            html="""
+                <p>Invalid credentials. Please check your App ID and Cert ID
+                in eBay API Settings and try again.</p>""",
+            success=False
+        )
 
     user_access = r.json()
     if user_access['token_type'] != 'User Access Token':
-        frappe.throw('Invalid parameters!')
+        return frappe.respond_as_web_page(
+            title='Authorization Failed',
+            html="""
+                <p><b>Invalid credentials.<b> Please check your App ID and Cert ID
+                in eBay API Settings and try again.</p>""",
+            success=False
+        )
     refresh_token = user_access['refresh_token']
     refresh_expiry = (
         datetime.datetime.now(datetime.timezone.utc)
